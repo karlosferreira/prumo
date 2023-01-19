3ad956314240ab3754822c9c2304194a46e1b2d3
karlosferreira <desenvolvimento.carlosferreira@gmail.com>
2023-01-19 16:43:22 -0300

    resolve fetch

--- ajax.php
+++ ajax.php
@@ -1,5 +1,6 @@
 <?php
 include('db.php');
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 if (empty($_POST['cnpj'])) {
     echo "<p class='warning'>Insira um cnpj válido.</p>";
@@ -28,8 +29,17 @@ if (isset($_POST['cnpj']) && !empty($_POST['cnpj'])) {
     $response = curl_exec($fetch);
     $result = json_decode($response);
 
-    $social_reason = $result->razao_social;
+    if(isset($result->status) && $result->status == 400) {
+        echo "<p class='info'>". $result->titulo .": " . $result->detalhes . "</p>";
+        exit;
+    }
 
+    if(isset($result->status) && $result->status == 429) {
+        echo "<p class='info'>". $result->titulo .": " . $result->detalhes . "</p>";
+        exit;
+    }
+
+    $social_reason = $result->razao_social;
     $social_capital = $result->capital_social;
 
     $address = $result->estabelecimento->tipo_logradouro . " " .
@@ -42,36 +52,44 @@ if (isset($_POST['cnpj']) && !empty($_POST['cnpj'])) {
 
     $porte = $result->porte->descricao;
 
-    if(!$result) {
-        echo "<p class='info'>Nada encontrado. Tente novamente</p>";
-        exit;
-    } else {
-        $query = sprintf("INSERT INTO `enterprises` (
-            `social_reason`, 
-            `social_capital`, 
-            `port_description`, 
-            `address`
-        ) VALUES (
-            '%s1',
-            '%s2',
-            '%s3',
-            '%s4'
-        )",
-            $social_reason,
-            $social_capital,
-            $porte,
-            $address
-        );
+    if( $result && !isset($result->status) ) {
+        $query_get = sprintf("SELECT * FROM `enterprises` WHERE `social_reason` = '%s'", $social_reason);
+        $query_fetch = mysqli_query($db, $query_get);
 
-        mysqli_query($db,$query);
-        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+        if ($query_fetch->num_rows <= 0) {
+            $query_post = sprintf("INSERT INTO `enterprises` (
+                `social_reason`, 
+                `social_capital`, 
+                `port_description`, 
+                `address`
+            ) VALUES (
+                '%s',
+                '%s',
+                '%s',
+                '%s'
+            )",
+                $social_reason,
+                $social_capital,
+                $porte,
+                $address
+            );
+            mysqli_query($db,$query_post);
+
+            echo "<h5>Dados da Empresa:</h5>";
+            echo "<P><b>Razão Social: </b>" . $social_reason . "</p>";
+            echo "<P><b>Capital Social: </b>" . $social_capital . "</p>";
+            echo "<P><b>Porte: </b>" . $porte . "</p>";
+            echo "<P><b>Endereço: </b>" . $address . "</p>";
+        } else {
+            while ($enterprise = mysqli_fetch_array($query_fetch)) {
+                echo "<h5>Dados da Empresa:</h5>";
+                echo "<P><b>Razão Social: </b>" . $enterprise['social_reason'] . "</p>";
+                echo "<P><b>Capital Social: </b>" . $enterprise['social_capital'] . "</p>";
+                echo "<P><b>Porte: </b>" . $enterprise['port_description'] . "</p>";
+                echo "<P><b>Endereço: </b>" . $enterprise['address'] . "</p>";
+            }
+        }
     }
-
-    echo "<h5>Dados da Empresa:</h5>";
-    echo "<P><b>Razão Social: </b>" . $social_reason . "</p>";
-    echo "<P><b>Capital Social: </b>" . $social_capital . "</p>";
-    echo "<P><b>Porte: </b>" . $porte. "</p>";
-    echo "<P><b>Endereço: </b>" . $address . "</p>";
 
     exit;
 }
