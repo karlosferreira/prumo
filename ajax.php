e726af9d0d0806c1361ec136f553324fd12d6504
karlosferreira <desenvolvimento.carlosferreira@gmail.com>
2023-01-19 13:03:41 -0300

    teste prumo - segunda parte

--- ajax.php
+++ ajax.php
@@ -2,23 +2,63 @@
 include('db.php');
  
 if (isset($_POST['cnpj']) && $_POST['cnpj']!="") {
-    $cnpj = $_POST['cnpj'];
+    $get_post = $_POST['cnpj'];
 
-    // path of the REST API URL
-    $url = "http://localhost/prumo/consulta-cnpj/".$cnpj;
-    $enterprise = curl_init($url);
+    $dont_dot = str_replace('.', '', $get_post);
+    $dont_hifen = str_replace('-', '', $dont_dot);
+    $cnpj = str_replace('/', '', $dont_hifen);
+
+    $url = "https://publica.cnpj.ws/cnpj/".$cnpj;
+    $fetch = curl_init($url);
     
-    curl_setopt($enterprise,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($fetch,CURLOPT_RETURNTRANSFER,true);
 
-    $response = curl_exec($enterprise);
+    $response = curl_exec($fetch);
     $result = json_decode($response);
+
+    $social_reason = $result->razao_social;
+
+    $social_capital = $result->capital_social;
+
+    $address = $result->estabelecimento->tipo_logradouro . " " .
+    $result->estabelecimento->logradouro . ", " .
+    $result->estabelecimento->numero . ". " .
+    $result->estabelecimento->complemento . ". " .
+    $result->estabelecimento->bairro . ", " .
+    $result->estabelecimento->cidade->nome . ", " .
+    $result->estabelecimento->estado->nome;
+
+    $porte = $result->porte->descricao;
+
 
     if(!$result) {
         echo "Nada encontrado. Tente novamente";
         exit;
+    } else {
+        $query = sprintf("INSERT INTO `enterprises` (
+            `social_reason`, 
+            `social_capital`, 
+            `port_description`, 
+            `address`
+        ) VALUES (
+            `%s1`,
+            `%s2`,
+            `%s3`,
+            `%s4`
+        )",
+        $social_reason,
+        $social_capital,
+        $porte,
+        $address);
+
+        mysqli_query($db,$query);
     }
 
-    echo "<h3>Enterprise Details:</h3>";
+    echo "<h4>Dados da Empresa:</h4>";
+    echo "<P><b>Razão Social: </b>" . $social_reason . "</p>";
+    echo "<P><b>Capital Social: </b>" . $social_capital . "</p>";
+    echo "<P><b>Porte: </b>" . $porte. "</p>";
+    echo "<P><b>Endereço: </b>" . $address . "</p>";
 
     exit;
 }
---
+++ api.php
@@ -0,0 +1,26 @@
+<?php
+    header("Content-Type:application/json");
+    include('db.php');
+
+    if (isset($_GET['enterprise_id']) && $_GET['enterprise_id']!="") {
+        $enterprise_id = $_GET['enterprise_id'];
+        $query = "SELECT * FROM `enterprises` WHERE enterprise_id=$enterprise_id";
+        $result = mysqli_query($db,$query);
+        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+
+        $cnpjData['enterprise_id'] = $row['enterprise_id'];
+        $cnpjData['social_reason'] = $row['social_reason'];
+        $cnpjData['social_capital'] = $row['social_capital'];
+        $cnpjData['port_description'] = $row['port_description'];
+        $cnpjData['address'] = $row['address'];
+
+        $response["status"] = "true";
+        $response["message"] = "Enterprise Details:";
+        $response["customers"] = $cnpjData;
+    } else {
+        $response["status"] = "false";
+        $response["message"] = "No data found!";
+    }
+
+    echo json_encode($response); 
+    exit;
